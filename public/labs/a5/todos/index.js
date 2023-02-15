38502054f5d34cbe89334a2829db3e2967784327
jamessenior642 <jvsenior02@gmail.com>
2023-02-15 14:59:56 -0500

    finished a5 lab

---
+++ public/labs/a5/todos/index.js
@@ -0,0 +1,12 @@
+// alert('Hello World!');
+
+import TodoList from "./TodoList.js";
+// eslint-disable-next-line no-undef
+$('#wd-todo').append(`
+   <div class="container">
+       <h1>Todo example</h1>
+       ${TodoList()}
+   </div>
+`);
+
+
