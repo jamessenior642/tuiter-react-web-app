b0eff7d387d249dc5a00e89c40065edf165969ff
jamessenior642 <jvsenior02@gmail.com>
2023-03-04 16:16:47 -0500

    finished a6

---
+++ src/tuiter/navigation-sidebar/index.js
@@ -0,0 +1,57 @@
+import React from "react";
+
+const NavigationSidebar = (
+    {
+        active = 'explore'
+    }
+) => {
+    return (
+        <div className="list-group">
+            <a className="list-group-item">
+                <i className="bi bi-twitter"></i>
+                <span className="d-done d-xl-inline d-none d-sm-none">&nbsp;Tuiter</span>
+            </a>
+            <a className={`list-group-item
+                    ${active === 'home'?'active':''}`}>
+                <i className="bi bi-house-fill"></i>
+                <span className="d-done d-xl-inline d-none d-sm-none">&nbsp;Home</span>
+            </a>
+            <a className={`list-group-item
+                    ${active === 'explore'?'active':''}`}>
+                <i className="bi bi-hash"></i>
+                <span className="d-done d-xl-inline d-none d-sm-none">&nbsp;Explore</span>
+            </a>
+            <a className={`list-group-item
+                    ${active === 'notifications'?'active':''}`}>
+                <i className="bi bi-bell-fill"></i>
+                <span className="d-done d-xl-inline d-none d-sm-none">&nbsp;Notifications</span>
+            </a>
+            <a className={`list-group-item
+                    ${active === 'messages'?'active':''}`}>
+                <i className="bi bi-chat-fill"></i>
+                <span className="d-done d-xl-inline d-none d-sm-none">&nbsp;Messages</span>
+            </a>
+            <a className={`list-group-item
+                    ${active === 'bookmarks'?'active':''}`}>
+                <i className="bi bi-bookmark-fill"></i>
+                <span className="d-done d-xl-inline d-none d-sm-none">&nbsp;Bookmarks</span>
+            </a>
+            <a className={`list-group-item
+                    ${active === 'lists'?'active':''}`}>
+                <i className="bi bi-list"></i>
+                <span className="d-done d-xl-inline d-none d-sm-none">&nbsp;Lists</span>
+            </a>
+            <a className={`list-group-item
+                    ${active === 'profile'?'active':''}`}>
+                <i className="bi bi-person-fill"></i>
+                <span className="d-done d-xl-inline d-none d-sm-none">&nbsp;Profile</span>
+            </a>
+            <a className={`list-group-item
+                    ${active === 'more'?'active':''}`}>
+                <i className="bi bi-three-dots"></i>
+                <span className="d-done d-xl-inline d-none d-sm-none">&nbsp;More</span>
+            </a>
+        </div>
+    );
+};
+export default NavigationSidebar;
---
+++ src/tuiter/who-to-follow-list/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import whoArray from './who.json';
+import WhoToFollowListItem
+    from "./who-to-follow-list-item";
+
+const WhoToFollowList = () => {
+    return(
+        <ul className="list-group">
+            <li className="fw-bold list-group-item">Who To Follow</li>
+            {
+                whoArray.map(who =>
+                    <WhoToFollowListItem
+                        key={who._id}
+                        who={who}/>
+                )
+            }
+        </ul>
+    );
+};
+
+export default WhoToFollowList;
