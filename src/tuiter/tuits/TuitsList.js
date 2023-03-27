bd689ff6252e969df28fc76f3cec01b786061866
jamessenior642 <jvsenior02@gmail.com>
2023-03-26 23:42:04 -0400

    finished a7

---
+++ src/tuiter/tuits/TuitsList.js
@@ -0,0 +1,16 @@
+import React from "react";
+import TuitItem from "./TuitItem";
+import { useSelector } from "react-redux";
+
+const TuitList = () => {
+    const tuitArray = useSelector(state => state.tuits)
+    return (
+        <ul className="list-group">
+            {
+                tuitArray.map(tuit =>
+                    <TuitItem key={tuit._id} tuit={tuit} />)
+            }
+        </ul>
+    );
+};
+export default TuitList;
